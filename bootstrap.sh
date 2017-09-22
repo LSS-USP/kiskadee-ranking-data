26251b20c373e6556edeb243a509001445d660fc
Athos Ribeiro <athoscr@fedoraproject.org>
2017-09-22 14:58:39 -0300

    Move patches to the ./patches subdirectory

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ if [ ! -d  juliet ]; then
   unzip -d juliet Juliet_Test_Suite_v1.2_for_C_Cpp.zip > /dev/null;
   pushd juliet;
   # This patch fixes some bugs in the provided python scripts to run tools on specific test cases
-  patch -p1 < ../00-fix-python-scripts.patch;
+  patch -p1 < ../patches/00-fix-python-scripts.patch;
   popd;
   rm Juliet_Test_Suite_v1.2_for_C_Cpp.zip;
 fi
