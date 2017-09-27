bc049d28d0e02494ec8aa39662830a9fdebdf924
Athos Ribeiro <athoscr@fedoraproject.org>
2017-09-27 15:29:57 -0300

    Fix frama-c and flawfinder analyses
    * Frama-C must analyze all complementary testcases in the same run
    * Running flawfinder in a directory is faster and seems to be the proper
    way to do it

--- run_analyses.sh
+++ run_analyses.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 C_FILES=c_testcases.list
 CPP_FILES=cpp_testcases.list
@@ -17,15 +17,6 @@ for mkfile in `cat $C_FILES $CPP_FILES | xargs -l -n1 dirname | sort -u`; do
 done
 printf "\tDone\n"
 
-# frama-c
-echo 'Running Frama-C analyses...'
-mkdir reports/frama-c;
-for c_file in `cat $C_FILES`; do
-  frama-c -val -value-log ew:reports/frama-c/framac.log -kernel-log ew:reports/frama-c/framac.log -cpp-extra-args='-Ijuliet/testcasesupport -DINCLUDEMAIN -U__cplusplus' juliet/testcasesupport/io.c $c_file > /dev/null;
-  cat reports/frama-c/framac.log >> reports/frama-c/all.log;
-done;
-printf "\tDone\n"
-
 # cppcheck
 echo 'Running cppcheck analyses...'
 mkdir reports/cppcheck;
@@ -37,10 +28,36 @@ cppcheck -I/home/athos/USP/pesquisa/experiments/juliet/testcasesupport --file-li
 rm cppcheck.list;
 printf "\tDone\n"
 
+# frama-c
+echo 'Running Frama-C analyses...'
+mkdir reports/frama-c;
+# frama-c must analyze complementary testcases ending with '[abcde].c' together
+partial_testcase_head_regex="[0-9]a\.c$"
+partial_testcase_regex="[0-9][bcde]\.c$"
+for c_file in `cat $C_FILES`; do
+  if [[ $c_file =~ $partial_testcase_regex ]]; then
+    continue;
+  elif [[ $c_file =~ $partial_testcase_head_regex ]]; then
+    target_files=`sed 's/a\.c$//'<<<$c_file`
+    frama-c -val -value-log ew:reports/frama-c/framac_temp.log -kernel-log ew:reports/frama-c/framac_temp.log -cpp-extra-args='-Ijuliet/testcasesupport -DINCLUDEMAIN -U__cplusplus' juliet/testcasesupport/io.c ${target_files}*.c > /dev/null;
+  else
+    frama-c -val -value-log ew:reports/frama-c/framac_temp.log -kernel-log ew:reports/frama-c/framac_temp.log -cpp-extra-args='-Ijuliet/testcasesupport -DINCLUDEMAIN -U__cplusplus' juliet/testcasesupport/io.c $c_file > /dev/null;
+  fi
+  cat reports/frama-c/framac_temp.log >> reports/frama-c/frama-c.log;
+  rm reports/frama-c/framac_temp.log
+done;
+printf "\tDone\n"
+
 # flawfinder
 echo 'Running flawfinder analyses...'
 mkdir reports/flawfinder;
-for source_file in `cat $C_FILES $CPP_FILES`; do
-  flawfinder juliet/testcasesupport/std_thread.c juliet/testcasesupport/io.c juliet/testcasesupport/main_linux.cpp $source_file >> reports/flawfinder/flawfinder.log;
-done;
+# we remove unwanted testcases to run flawfinder in the whole testsuite (this is faster)
+cp -R juliet juliet_backup;
+find  juliet/testcases | grep '/CWE[^/]*w32.*\.c$\|/CWE[^/]*wchar_t.*\.c$' | sort -u > c_testcases.toremove;
+find  juliet/testcases | grep '/CWE[^/]*w32.*\.cpp$\|/CWE[^/]*wchar_t.*\.cpp$' | sort -u > cpp_testcases.toremove;
+sort $C_FILES $C_FILES c_testcases.toremove | uniq -u | xargs rm
+sort $CPP_FILES $CPP_FILES cpp_testcases.toremove | uniq -u | xargs rm
+flawfinder juliet/testcasesupport -S juliet/testcases > reports/flawfinder/flawfinder.log;
+rm -rf juliet
+mv juliet_backup juliet
 printf "\tDone\n"
