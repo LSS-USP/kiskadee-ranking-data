65d40e81401fee615cab49f8c7a210b82f36c286
Athos Ribeiro <athoscr@fedoraproject.org>
2017-10-06 16:40:27 -0300

    Improve data report format

--- collect_data.sh
+++ collect_data.sh
@@ -5,33 +5,42 @@ CPP_FILES=cpp_testcases.list
 
 printf "Total number of test cases in Juliet: %s\n" \
 `find juliet/testcases -regex '.*[0-9a]\.c[p]*$' | wc -l`
+echo
 
 printf "Number of C test cases in Juliet: %s\n" \
 `find juliet/testcases -regex '.*[0-9a]\.c$' | wc -l`
+echo
 
 printf "Number of C++ test cases in Juliet: %s\n" \
 `find juliet/testcases -regex '.*[0-9a]\.cpp$' | wc -l`
+echo
 
 printf "Number of CWEs covered in juliet: %s\n" \
 `ls juliet/testcases | wc -l`
+echo
 
 printf "Number of C test cases used in this experiment: %s\n" \
 `cat $C_FILES | grep '.*[0-9a]\.c$' | wc -l`
-echo "\tthis excludes windows specific test cases"
+printf "\tthis excludes windows specific test cases\n"
+echo
 
 printf "Number of C++ test cases used in this experiment: %s\n" \
 `cat $CPP_FILES | grep '.*[0-9a]\.cpp$' | wc -l`
-echo "\tthis excludes windows specific test cases"
+printf "\tthis excludes windows specific test cases\n"
+echo
 
 printf "Number of different Juliet CWEs analyzed in this experiment: %s\n" \
 `cat c_testcases.list cpp_testcases.list | cut -d / -f3 | sort -u | wc -l`
-echo "\tthis excludes CWEs containing only windows specific test cases"
+printf "\tthis excludes CWEs containing only windows specific test cases\n"
+echo
 
 printf "Number of CWEs convered on the C test cases used %s\n" \
 `cat c_testcases.list | cut -d / -f3 | sort -u | wc -l`
+echo
 
 printf "Number of CWEs convered on the C++ test cases used %s\n" \
 `cat cpp_testcases.list | cut -d / -f3 | sort -u | wc -l`
+echo
 
 echo 'Version of static analyzers:'
 # This is RPM distro specific and could be improved
@@ -45,5 +54,6 @@ printf "\tcppcheck: %s\n" \
 # scan-build
 printf "\tscan-build (Clang Analyzer): %s\n" \
 `rpm -qi clang-analyzer | grep Version | awk '{ print $3 }'`
+echo
 
 python firehose_report_parser.py stats
