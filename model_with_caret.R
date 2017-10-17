fd9a3ec51a786ed22bc97eb30091c50d7cfa5978
Athos Ribeiro <athoscr@fedoraproject.org>
2017-10-17 03:22:51 -0200

    Improve R models

--- model_with_caret.R
+++ model_with_caret.R
@@ -1,12 +1,21 @@
 library(caret)
 raw_data <- read.csv("features.csv", header=TRUE, sep=",")
-train_control <- trainControl(method="cv", number=10)
-# ??
-# grid <- expand.grid(.fL=c(0), .usekernel=c(FALSE))
-#grid <- expand.grid(nIter=5, method='')
-#model <- train(label~tool_name+severity+redundancy_level+neighbors+category, data=raw_data, trControl=train_control, method="nb", tuneGrid=grid)
-# adaboost uses the fastadaboostpackage
-# the nIter param sets the number of trees to train
-#model <- train(label~tool_name+severity+redundancy_level+neighbors+category, data=raw_data, trControl=train_control, method="adaboost", tuneGrid=grid)
-model <- train(label~tool_name+severity+redundancy_level+neighbors+category+clang_analyzer+frama_c+cppcheck+warnings_in_this_file, data=raw_data, trControl=train_control, method="adaboost")
+
+inTraining <- createDataPartition(raw_data$label, p = .75, list = FALSE)
+training_data <- raw_data[ inTraining,]
+testing_data  <- raw_data[-inTraining,]
+
+train_control <- trainControl(method="cv", number=10, repeats=10)
+# grid <- expand.grid(nIter=10, method='Adaboost.M1')
+set.seed(825)
+model <- train(label~tool_name+severity+redundancy_level+neighbors+category+clang_analyzer+frama_c+cppcheck+warnings_in_this_file, data=training_data, trControl=train_control, method="adaboost")#, tuneGrid=grid)
 print(model)
+
+
+modelImp <- varImp(model, scale = FALSE)
+print(modelImp)
+ggplot(modelImp)
+
+predict(model, newdata = testing_data)
+predict(model, newdata = testing_data, type="prob")
+ggplot(model)
