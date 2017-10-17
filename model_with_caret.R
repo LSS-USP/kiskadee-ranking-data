d9328b5ff6f13f964167cf56953fd875745a9ecb
Athos Ribeiro <athoscr@fedoraproject.org>
2017-10-17 03:45:39 -0200

    Use repeated cross-validation

--- model_with_caret.R
+++ model_with_caret.R
@@ -5,7 +5,7 @@ inTraining <- createDataPartition(raw_data$label, p = .75, list = FALSE)
 training_data <- raw_data[ inTraining,]
 testing_data  <- raw_data[-inTraining,]
 
-train_control <- trainControl(method="cv", number=10, repeats=10)
+train_control <- trainControl(method="repeatedcv", number=10, repeats=10)
 # grid <- expand.grid(nIter=10, method='Adaboost.M1')
 set.seed(825)
 model <- train(label~tool_name+severity+redundancy_level+neighbors+category+clang_analyzer+frama_c+cppcheck+warnings_in_this_file, data=training_data, trControl=train_control, method="adaboost")#, tuneGrid=grid)
