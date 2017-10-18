d31d45a7b59f3ea716fa290fcf5bf663f2fcdbae
Athos Ribeiro <athoscr@fedoraproject.org>
2017-10-18 01:15:13 -0200

    Improve model

--- model_with_caret.R
+++ model_with_caret.R
@@ -1,4 +1,5 @@
 library(caret)
+print("LOG: BEGIN")
 raw_data <- read.csv("features.csv", header=TRUE, sep=",")
 
 inTraining <- createDataPartition(raw_data$label, p = .75, list = FALSE)
@@ -8,21 +9,31 @@ testing_data  <- raw_data[-inTraining,]
 train_control <- trainControl(method="repeatedcv", number=10, repeats=10)
 # grid <- expand.grid(nIter=10, method='Adaboost.M1')
 set.seed(825)
-model <- train(label~tool_name+severity+redundancy_level+neighbors+category+clang_analyzer+frama_c+cppcheck+warnings_in_this_file+language, data=training_data, trControl=train_control, method="adaboost")#, tuneGrid=grid)
-print(model)
+model <- train(label~tool_name+severity+redundancy_level+neighbors+category+clang_analyzer+frama_c+cppcheck+warnings_in_this_file+language, data=training_data, trControl=train_control, method="adaboost") #, tuneGrid=grid)
+print("LOG: Model")
+model
 
 
 modelImp <- varImp(model, scale = FALSE)
-print(modelImp)
+modelImp
 ggplot(modelImp)
 
 predictions = predict(model, newdata = testing_data)
 probabilities = predict(model, newdata = testing_data, type="prob")
 
-confusion_matrix = confusionMatrix(pred, testing_data$label)
+confusion_matrix = confusionMatrix(predictions, testing_data$label, positive="true", mode="prec_recall")
 
-print(predictions)
-print(probabilities)
-print(confusion_matrix)
+print("LOG: Predictions")
+predictions
+print("LOG: Labels")
+testing_data$label
+print("LOG: Probabilities")
+probabilities
+print("LOG: Confusion Matrix")
+confusion_matrix
 
+print("LOG: Plotting...")
 ggplot(model)
+print("LOG: Saving...")
+save.image()
+print("LOG: END, model saved at .RData")
